b9b8ffb0847cc3b6cfae429f4c1547bf0e8a1ab4
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-04-24 22:58:42 +0530

    Create Pages

---
+++ food-del/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-content">
+      <h2>Order your favourite food here</h2>
+      <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredinets and culinary expertise. Our delicious meal at a time.</p>
+      <button>View Menu </button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- food-del/frontend/src/pages/Home/Home.jsx
+++ food-del/frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 const Home = () => {
   return (
     <div>
-      
+      <Header/>
     </div>
   )
 }
