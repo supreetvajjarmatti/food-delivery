8f3d86506f281be3d4a7e28e18d91c48ac0df679
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-04-26 23:37:18 +0530

    Create Footer

--- food-del/frontend/src/App.jsx
+++ food-del/frontend/src/App.jsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
+    <>
     <div className='app'>
       <Navbar/>
       <Routes>
@@ -16,6 +18,8 @@ const App = () => {
       </Routes>
       
     </div>
+    <Footer/>
+    </>
   )
 }
 
---
+++ food-del/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus delectus laudantium ab incidunt. Natus iusto eius magnam provident. Quos voluptate architecto iusto pariatur earum provident inventore eligendi rerum recusandae.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomoto.com</li>
+                </ul>
+            </div>
+        </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  )
+}
+
+export default Footer
