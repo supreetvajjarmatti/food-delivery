8b5b068f5c23fe97410fb80c7b221e9c055eb9f4
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-04-26 17:37:23 +0530

    Add to cart

--- food-del/frontend/src/context/StoreContext.jsx
+++ food-del/frontend/src/context/StoreContext.jsx
@@ -1,9 +1,21 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
+
+    const [cartItems,setCartItems] = useState({});
+
+    const addToCart = (itemId) => {
+        if (!cartItems[itemId]){
+            setCartItems((prev)=>({...prev,[itemId]:1}))
+        }
+        else{
+            setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+        }
+    }
+
     const contextValue = {
         food_list
     };
