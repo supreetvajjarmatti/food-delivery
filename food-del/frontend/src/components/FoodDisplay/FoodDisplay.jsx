a7d49fe30bd80f929382a4700bc35f00009867bf
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-04-26 17:12:12 +0530

    Created food list

--- food-del/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ food-del/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
@@ -9,7 +10,7 @@ const FoodDisplay = ({category}) => {
         <h2>Top dishes near  you</h2>
         <div className="food-display-list">
             {food_list.map((item,index)=>{
-                return
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
             })}
         </div>
       
--- food-del/frontend/src/components/FoodItem/FoodItem.jsx
+++ food-del/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './FoodItem.css'
+import { assets } from '../../assets/assets'
 
-const FoodItem = () => {
+const FoodItem = ({id,name,price,description,image}) => {
   return (
-    <div>
+    <div className='food-item'>
+      <div className="food-item-img-container">
+        <img src={image} alt="" className="food-item-image" />
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-name-rating">
+          <p>{name}</p>
+          <img src={assets.rating_starts} alt="" />
+        </div>
+        <p className="food-item-desc">
+          {description}
+        </p>
+        <p className="food-item-price">
+         ${price}
+        </p>
+      </div>
       
     </div>
   )
