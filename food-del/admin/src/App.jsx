6c4fba332d2d038d691983700a8659537578fd56
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-01 18:19:07 +0530

    Admin Navbar

--- food-del/admin/src/App.jsx
+++ food-del/admin/src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/sidebar/sidebar'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+     <Navbar/>
+     <hr/>
+     <div className="app-content">
+      <Sidebar/>
+     </div>
       
-      <hr />
+  
       
-      <div className="app-content">
-        <Sidebar/>
-      </div>
+      
     </div>
   )
 }
--- food-del/admin/src/components/Sidebar/Sidebar.jsx
+++ food-del/admin/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-import './Sidebar.css'
+import './sidebar.css'
+import { assets } from '../../assets/assets'
 
-const Sidebar = () => {
+const sidebar = () => {
   return (
-    <div>
+    <div className='sidebar'>
+        <div className="sidebar-options">
+            <div className="sidebar-option">
+                <img src={assets.add_icon} alt="" />
+                <p>Add Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" />
+                <p>List Items</p>
+            </div>
+            <div className="sidebar-option">
+                <img src={assets.order_icon} alt="" />
+                <p>Order</p>
+            </div>
+            
+        </div>
       
     </div>
   )
 }
 
-export default Sidebar;
+export default sidebar
