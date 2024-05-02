bd6128e28755674de22d0f1fa5c02d7ef38724cb
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-02 11:14:24 +0530

    Add panel Add Items functionality Completed

--- food-del/admin/src/pages/Add/Add.jsx
+++ food-del/admin/src/pages/Add/Add.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import './Add.css'
 import { assets } from '../../assets/assets'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const Add = () => {
+    const url = "http://localhost:4000";
   const [image, setImage] = useState(null);
   const [data, setData] = useState({
     name: "",
@@ -12,17 +15,38 @@ const Add = () => {
   });
 
   const onChangeHandler = (event) => {
-    const { name, value } = event.target;
+    const name = event.target.name;
+    const value = event.target.value
     setData(prevData => ({ ...prevData, [name]: value }));
   }
+ const onSubmitHandler = async (event) =>{
+    event.preventDefault()
+    const formData = new FormData();
+    formData.append('name',data.name)
+    formData.append('description',data.description)
+    formData.append('price',Number(data.price))
+    formData.append('category',data.category)
+    formData.append('image',image)
+    const response = await axios.post(`${url}/api/food/add`,formData);
+    if(response.data.success) {
+       setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+      })  
+      setImage(false)
+      toast.success(response.data.message)
+    }
+    else{
+        toast.error(response.data.message)
+    }
+ }
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
-
+  
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler}> 
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
