9b52cfa8536c117938111759a70c5ec23abcbd5b
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-03 10:07:43 +0530

    Admin Panel List Items

--- food-del/admin/src/App.jsx
+++ food-del/admin/src/App.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/sidebar/sidebar'
-import { Route, Routes } from 'react-router-dom'
-import Add from './pages/Add/Add'
-import List from './pages/List/List'
-import Orders from './pages/Orders/Orders'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/sidebar/sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
-      <ToastContainer/>
-     <Navbar/>
-     <hr/>
-     <div className="app-content">
-      <Sidebar/>
-      <Routes>
-        <Route path='/add' element={<Add/>}/>
-        <Route path='/list' element={<List/>}/>
-        <Route path='/Orders' element={<Orders/>}/>
-      </Routes>
-     </div>
-      
-  
-      
-      
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/Orders" element={<Orders />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- food-del/admin/src/pages/List/List.jsx
+++ food-del/admin/src/pages/List/List.jsx
@@ -1,12 +1,62 @@
-import React from 'react'
-import './List.css'
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return (
-    <div>
-    
-    </div>
-  )
-}
+  const url = "http://localhost:4000";
 
-export default List
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const removeFood = async(FoodId)=>{
+    const response = await axios.post(`${url}/api/food/remove`,{id:FoodId})
+  await fetchList()
+  if(response.data.success){
+    toast.success(response.data.message)
+  }else{
+    toast.error("Error")
+  }
+  }
+
+  useEffect(() => {
+    fetchList();
+  },[])
+
+  return (
+  <div className="list add flex-col">
+      <p>All Food List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index) => {
+          return (
+            <div key={index} className="list-table-format">
+              <img src={`${url}/images/`+item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={()=>removeFood(item._id)} className="cursor">X</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
