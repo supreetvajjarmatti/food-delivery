f506a8129bb4c335631ccc477952f61a8a8d5262
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-05 01:06:26 +0530

    User Register

---
+++ food-del/backend/controllers/userController.js
@@ -0,0 +1,57 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"; // used for encrypt the password
+import validator from "validator";
+
+//login user
+
+const loginUser = async (req, res) => {
+
+
+};
+
+const creatToken = (id) => {
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
+
+
+//register user
+
+const registerUser = async (req, res) => {
+    const { name, password, email } = req.body;
+    try {
+        //checking is user already exists
+        const exists = await userModel.findOne({ email });
+        if (exists) {
+          return res.json({ success: false, message: "User already exist" });
+        }
+        //validating email format & strong password
+        if(!validator.isEmail(email)) {
+        return res.json({ success: false, message: "Please enter valid email" })
+        }
+    //password length must be greater than 8
+        if(password.length<8){
+            return res.json({success:false,message:"Please enter a strong password"})
+        }
+    //hashing user password.
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+    
+    const newUser = new userModel({
+        name:name,
+        email:email,
+        password:hashedPassword
+    })
+    
+    const user = await newUser.save()
+    const token = creatToken(user._id)
+    res.json({success:true,token});
+    
+      } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+      }
+    };
+
+
+export { loginUser, registerUser };
--- food-del/backend/server.js
+++ food-del/backend/server.js
@@ -3,6 +3,8 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
+import userRouter from "./routes/userRoute.js"
+import 'dotenv/config'
 
 
 
@@ -23,6 +25,7 @@ connectDB();
 //API endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 //http method to request the data
 app.get("/",(req,res)=>{
