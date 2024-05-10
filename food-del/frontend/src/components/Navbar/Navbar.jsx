c7843857660990e639fe5ef7edecabd96c8339bf
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-10 09:04:57 +0530

    Create user authentication (login & registration) feature

--- food-del/frontend/src/components/Navbar/Navbar.jsx
+++ food-del/frontend/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,22 @@
 import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 
 const Navbar = ({setShowLogin}) => {
     const [menu, setMenu] = useState("home");
 
-    const {getTotalCartAmount} = useContext(StoreContext);
+    const {getTotalCartAmount, token, setToken} = useContext(StoreContext);
+
+    const navigate = useNavigate();
+
+    const logout = () =>{
+        localStorage.removeItem("token")
+        setToken("");
+        navigate("/")
+
+    }
 
 
 
@@ -27,7 +36,15 @@ const Navbar = ({setShowLogin}) => {
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className={getTotalCartAmount()===0?"":"dot"}></div>
             </div>
-            <button onClick={()=>setShowLogin(true)}>sign in</button>
+            {!token?<button onClick={()=>setShowLogin(true)}>sign in</button>:<div className='navbar-profile'> <img src={assets.profile_icon} alt=""/>
+            <ul className="nav-profile-dropdown">
+                <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+                <hr />
+                <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
+            </ul>
+
+            </div>}
+            
         </div>
       
     </div>
--- food-del/frontend/src/context/StoreContext.jsx
+++ food-del/frontend/src/context/StoreContext.jsx
@@ -1,10 +1,13 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
+  const url = "http://localhost:4000"
+  const [token,setToken] =useState("")
+
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -29,13 +32,22 @@ export const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      setToken(localStorage.getItem("token"))
+    }
+  },[])
+
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
     removeFromCart,
-    getTotalCartAmount
+    getTotalCartAmount,
+    url,
+    token,
+    setToken
   };
 
   return (
