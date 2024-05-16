69fb661e5a684903a885123a3fefa853d3ec2e0f
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-16 23:47:45 +0530

    Create user order pageCreate user order page

--- food-del/backend/routes/orderRoute.js
+++ food-del/backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import authMiddleware from "../middleware/auth.js"
-import { placeOrder, verifyOrder } from '../controllers/orderController.js'
+import { placeOrder, userOrders, verifyOrder } from '../controllers/orderController.js'
 
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place",authMiddleware,placeOrder)
 orderRouter.post("/verify",verifyOrder)
+orderRouter.post("/userOrders",authMiddleware,userOrders)
 
 export default orderRouter
