909f12b69f7660768ebbb079e720ba755a6e07a7
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-05-17 13:02:44 +0530

    Create order update feature

--- food-del/backend/controllers/orderController.js
+++ food-del/backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //placing user order for frontend
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "http://localhost:5174";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
