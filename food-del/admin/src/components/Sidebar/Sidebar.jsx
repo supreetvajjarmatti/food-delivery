12faf7f533d55e3b46514ae632313a61d04bf400
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-06-23 08:24:01 +0530

    Error Occured

--- food-del/admin/src/components/Sidebar/Sidebar.jsx
+++ food-del/admin/src/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,27 @@
-import React from 'react'
-import './sidebar.css'
-import { assets } from '../../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import './sidebar.css';
+import { assets } from '../../assets/assets';
+import { NavLink } from 'react-router-dom';
 
-const sidebar = () => {
+const Sidebar = () => {
   return (
     <div className='sidebar'>
-        <div className="sidebar-options">
-            <NavLink to='/add' className="sidebar-option">
-                <img src={assets.add_icon} alt="" />
-                <p>Add Items</p>
-            </NavLink>
-            <NavLink to='/list' className="sidebar-option">
-                <img src={assets.order_icon} alt="" />
-                <p>List Items</p>
-            </NavLink>
-            <NavLink to='/orders' className="sidebar-option">
-                <img src={assets.order_icon} alt="" />
-                <p>Order</p>
-            </NavLink>
-            
-        </div>
-      
+      <div className="sidebar-options">
+        <NavLink to='/add' className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Order</p>
+        </NavLink>
+      </div>
     </div>
-  )
+  );
 }
 
-export default sidebar
+export default Sidebar;
---
+++ food-del/admin/src/components/Sidebar/Testcomponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TestComponent = () => {
+  return <div>Test Component</div>;
+};
+
+export default TestComponent;
