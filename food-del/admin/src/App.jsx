e311fdea368c1d95e2bc39a84dfc0b5f4f92ec80
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-06-23 08:56:28 +0530

    Error Occured

--- food-del/admin/src/App.jsx
+++ food-del/admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import Sidebar from './components/sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import { Route, Routes } from "react-router-dom";
 import Add from "./pages/Add/Add";
 import List from "./pages/List/List";
--- food-del/admin/src/components/Sidebar/Sidebar.jsx
+++ food-del/admin/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './sidebar.css';
+import './Sidebar.css';
 import { assets } from '../../assets/assets';
 import { NavLink } from 'react-router-dom';
 
