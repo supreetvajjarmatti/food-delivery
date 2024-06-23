b49f4176fe906ab8262a8d55e3cc506369d7cbe8
supreetvajjarmatti <supreetvajjaramatti@gmail.com>
2024-06-23 07:29:33 +0530

    changes to bcrypt to bcryptjs

--- food-del/backend/controllers/userController.js
+++ food-del/backend/controllers/userController.js
@@ -1,10 +1,9 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"; // used for encrypt the password
+import bcrypt from "bcryptjs"; // Change from bcrypt to bcryptjs
 import validator from "validator";
 
-//login user
-
+// Login user
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -13,15 +12,17 @@ const loginUser = async (req, res) => {
     if (!user) {
       return res.json({ success: false, message: "User Doesn't exist" });
     }
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.json({ success: false, message: "Invalid credentials" });
     }
+
     const token = creatToken(user._id);
     res.json({ success: true, token });
   } catch (error) {
     console.log(error);
-    res.json({success:false,message:"Error"})
+    res.json({ success: false, message: "Error" });
   }
 };
 
@@ -29,35 +30,37 @@ const creatToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
-//register user
-
+// Register user
 const registerUser = async (req, res) => {
   const { name, password, email } = req.body;
   try {
-    //checking is user already exists
+    // Checking if user already exists
     const exists = await userModel.findOne({ email });
     if (exists) {
-      return res.json({ success: false, message: "User already exist" });
+      return res.json({ success: false, message: "User already exists" });
     }
-    //validating email format & strong password
+
+    // Validating email format & strong password
     if (!validator.isEmail(email)) {
-      return res.json({ success: false, message: "Please enter valid email" });
+      return res.json({ success: false, message: "Please enter a valid email" });
     }
-    //password length must be greater than 8
+
+    // Password length must be greater than 8
     if (password.length < 8) {
       return res.json({
         success: false,
         message: "Please enter a strong password",
       });
     }
-    //hashing user password.
+
+    // Hashing user password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUser = new userModel({
       name: name,
       email: email,
-      password: hashedPassword
+      password: hashedPassword,
     });
 
     const user = await newUser.save();
